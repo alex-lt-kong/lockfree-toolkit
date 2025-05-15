4919e6e26360167e132b501a6eda352b0306e6fb
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-05-15 21:10:56 +0800

    update and more comments...

--- ringbuffer/rightbuffer-spsc-impl-test.cpp
+++ ringbuffer/rightbuffer-spsc-impl-test.cpp
@@ -5,6 +5,7 @@
 
 #include <print>
 #include <thread>
+#include <format>
 
 using namespace PoC::LockFree;
 
@@ -12,6 +13,7 @@ template<typename T>
 class TestClassNotCopyable {
 public:
     TestClassNotCopyable() { m_data = new T[1]; }
+
     explicit TestClassNotCopyable(std::size_t sz) { m_data = new T[sz]; }
 
     TestClassNotCopyable(const TestClassNotCopyable &) = delete;
@@ -33,8 +35,11 @@ public:
     }
 
     ~TestClassNotCopyable() { delete[] m_data; }
+
     T get(int idx) const { return m_data[idx]; }
+
     void set(int idx, const T &val) const { m_data[idx] = val; }
+
     bool empty() const { return m_data == nullptr; }
 
 private:
@@ -168,38 +173,6 @@ TEST(RingBufferSPSC, SingleThreadConsumeUnderflow) {
     }
 }
 
-TEST(RingBufferSPSC, SPSCSimple) {
-    // sz cant be too small as the context switch can happen very infrequently
-    // (experiments show iter_size / 4 is too small)
-    constexpr std::size_t iter_size = INT32_MAX;
-    constexpr std::size_t sz = iter_size / 2;
-    RingBufferSPSC<int> rb(sz);
-    auto producer = [&] {
-        for (std::size_t i = 0; i < iter_size; ++i) {
-            EXPECT_TRUE(rb.enqueue(i));
-        }
-    };
-    auto consumer = [&] {
-        std::size_t dequeue_count = 0;
-        while (dequeue_count < iter_size) {
-            int ele;
-            if (rb.dequeue(ele)) {
-                EXPECT_EQ(ele, dequeue_count);
-                ++dequeue_count;
-            }
-        }
-    };
-
-    std::thread thread1(producer);
-    std::thread thread2(consumer);
-
-    thread1.join();
-    thread2.join();
-
-    int ele;
-    EXPECT_FALSE(rb.dequeue(ele));
-}
-
 TEST(RingBufferSPSC, SPSCCantCopy) {
     constexpr std::size_t sz = INT16_MAX / 2;
     constexpr std::size_t iter_size = INT16_MAX;
@@ -235,5 +208,51 @@ TEST(RingBufferSPSC, SPSCCantCopy) {
     thread2.join();
 
     TestClassNotCopyable<std::pair<int, int> > ele;
+    EXPECT_FALSE(rb.dequeue(ele));
+}
+
+TEST(RingBufferSPSC, SPSCDoesNotForceStdMove) {
+    constexpr std::size_t sz = 10;
+    RingBufferSPSC<int> rb(sz);
+    for (int i = 0; i < sz; ++i) {
+        EXPECT_TRUE(rb.enqueue(i));
+    }
+    for (int i = 0; i < sz; ++i) {
+        int val;
+        EXPECT_TRUE(rb.dequeue(val));
+        EXPECT_EQ(val, i);
+    }
+}
+
+
+TEST(RingBufferSPSC, SPSCSimple) {
+    // sz cant be too small as the context switch can happen very infrequently
+    // (experiments show iter_size / 4 is too small)
+    constexpr std::size_t iter_size = INT32_MAX;
+    constexpr std::size_t sz = iter_size / 2;
+    RingBufferSPSC<int> rb(sz);
+    auto producer = [&] {
+        for (std::size_t i = 0; i < iter_size; ++i) {
+            EXPECT_TRUE(rb.enqueue(i));
+        }
+    };
+    auto consumer = [&] {
+        std::size_t dequeue_count = 0;
+        while (dequeue_count < iter_size) {
+            int ele;
+            if (rb.dequeue(ele)) {
+                EXPECT_EQ(ele, dequeue_count);
+                ++dequeue_count;
+            }
+        }
+    };
+
+    std::thread thread1(producer);
+    std::thread thread2(consumer);
+
+    thread1.join();
+    thread2.join();
+
+    int ele;
     EXPECT_FALSE(rb.dequeue(ele));
 }
