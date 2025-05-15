c1c3163a08893b552bc9e73e652b3f96536b4eb3
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-05-15 23:00:44 +0800

    add README and formatting

---
+++ ringbuffer/README.md
@@ -0,0 +1,40 @@
+# Lockfree SPSCQueue
+
+## A few interesting questions
+
+- Q: What are `std::memory_order_relaxed`, `std::memory_order_acquire` and
+  `std::memory_order_release`? Are they memory barriers?
+- A:
+    - Not really, they are C++'s memory ordering semantics. The memory orderings
+      prevent the compiler and CPU from reordering memory
+      accesses.<sup>[[Misunderstanding in Acquire/Release. Memory ordering](https://users.rust-lang.org/t/misunderstanding-in-acquire-release-memory-ordering/114010/4)]</sup>
+    - whether a compiler needs to use any barrier instructions to implement the
+      C++ semantics in the asm is just an implementation detail, quite separate
+      from the correctness of the memory-order specified for
+      operations.<sup>[[Reverse the [memory-barriers] [memory-order] synonym situation so questions about C++ std::memory_order can be tagged memory-order, not barriers](https://meta.stackoverflow.com/questions/419898/reverse-the-memory-barriers-memory-order-synonym-situation-so-questions-abou)]</sup>
+    - Microsoft Copilot's summary: Traditional memory barriers (like `mfence` in
+      x86 assembly) enforce explicit ordering at the hardware level, whereas C++
+      atomic operations offer a higher-level abstraction that guides compiler
+      optimizations while ensuring thread synchronization.
+
+
+- Q: If we target x86 architecture only, is memory barrier needed?
+- A: Strictly speaking the answer is no:
+    - Among the commonly used architectures, x86-64 processors have the
+      strongest memory order, but may still defer memory store instructions
+      until after memory load
+      instructions. <sup>[[Memory ordering](https://en.wikipedia.org/wiki/Memory_ordering)]</sup>
+    - You don't need memory barriers to implement an SPSC queue for x86. You can
+      do a relaxed store to the queue followed by a release write to
+      producer_idx. As long as consumer begins with an acquire load from
+      producer_idx it is guaranteed to see all stores to the queue memory before
+      producer_idx, according to the happens before ordering. There are no
+      memory barriers on x86 for acquire/release
+      semantics. <sup>[[Avoiding expensive memory barriers](https://groups.google.com/g/mechanical-sympathy/c/tHe1ARCvxDU/m/QTvpMtTlAQAJ)]</sup>
+    - One important point to remember here is that we are talking about
+      instructions in program order that the compiler emitted during the
+      compilation phase. The compiler itself is also free to reorder
+      instructions in any way it sees fit to optimize your code! What this means
+      is that it is not just the processor we need to worry about, but also the
+      compiler. Using compiler memory barriers can help in ensuring
+      this.<sup>[[Memory Barriers on x86](https://www.asrivas.me/blog/memory-barriers-on-x86/)]</sup>
--- ringbuffer/ringbuffer-spsc-impl.h
+++ ringbuffer/ringbuffer-spsc-impl.h
@@ -6,7 +6,8 @@
 
 /* Refer to
  * - https://github.com/facebook/folly/blob/main/folly/ProducerConsumerQueue.h
- * - https://github.com/cameron314/readerwriterqueue/blob/master/readerwritercircularbuffer.h
+ * -
+ * https://github.com/cameron314/readerwriterqueue/blob/master/readerwritercircularbuffer.h
  */
 
 /* Notes:
@@ -18,81 +19,82 @@
  * - A write-release guarantees that all preceding code completes before the
  * releasing write
  */
-namespace PoC::LockFree {
-    template<typename T>
-    class RingBufferSPSC {
-    private:
-        /*
-          Head/tail could be confusing, usually for FIFO queue, head is when
-          element comes out, tail is where element gets in. To visualize it,
-          you see that without wrapping around, Head is on the left while Tail
-          is on the right:
-                  +----+----+----+----+----+----+
-                  |    |  B |  C |  D |    |    |
-                  +----+----+----+----+----+----+
-                         ↑          ↑
-                        Head       Tail
-         * */
-        const size_t m_capacity;
-        std::vector<T> m_buffer;
-        std::atomic<size_t> m_write_ptr; // Points to the NEXT available position to write, i.e., the tail end of the queue
-        std::atomic<size_t> m_read_ptr;  // Points to the NEXT available position to read, i.e., the head end of the queue
-    public:
-        // we want to distinguish between buffer empty (tail == head) and buffer
-        // full (tail + 1 == head), so we need the allocate capacity+1
-        explicit RingBufferSPSC(const size_t capacity) : m_capacity(
-                capacity + 1), m_buffer(capacity + 1), m_write_ptr(0),
-                                                         m_read_ptr(0) {}
+namespace LockFree {
+template <typename T> class RingBufferSPSC {
+private:
+  /*
+    Head/tail could be confusing, usually for FIFO queue, head is when
+    element comes out, tail is where element gets in. To visualize it,
+    you see that without wrapping around, Head is on the left while Tail
+    is on the right:
+            +----+----+----+----+----+----+
+            |    |  B |  C |  D |    |    |
+            +----+----+----+----+----+----+
+                   ↑          ↑
+                  Head       Tail
+   * */
+  const size_t m_capacity;
+  std::vector<T> m_buffer;
+  std::atomic<size_t> m_write_ptr; // Points to the NEXT available position to
+                                   // write, i.e., the tail end of the queue
+  std::atomic<size_t> m_read_ptr;  // Points to the NEXT available position to
+                                   // read, i.e., the head end of the queue
+public:
+  // we want to distinguish between buffer empty (tail == head) and buffer
+  // full (tail + 1 == head), so we need the allocate capacity+1
+  explicit RingBufferSPSC(const size_t capacity)
+      : m_capacity(capacity + 1), m_buffer(capacity + 1), m_write_ptr(0),
+        m_read_ptr(0) {}
 
-        // We need to define a new type U to make enqueue() work for lvalue
-        // a new type U makes it a "universal reference":
-        // https://isocpp.org/blog/2012/11/universal-references-in-c11-scott-meyers
-        // Buy T&& is not a universal reference while U&& is?
-        // T&& is NOT a universal reference because T is already a fixed type (not deduced).
-        // U&& IS a universal reference because U is deduced dynamically.
-        template<typename U>
-        // Conversion (std::convertible_to<U, T>) creates a new object, so no reference is needed.
-        // Assignment (std::assignable_from<T&, U>) modifies an existing object, so T& is required.
-        // requires std::convertible_to<U, T>
-        requires std::assignable_from<T &, U>
-        bool enqueue(U &&item) {
-            size_t tail = m_write_ptr.load(std::memory_order_relaxed);
-            const size_t next_tail = (tail + 1) % m_capacity;
+  // We need to define a new type U to make enqueue() work for lvalue
+  // a new type U makes it a "universal reference":
+  // https://isocpp.org/blog/2012/11/universal-references-in-c11-scott-meyers
+  // Buy T&& is not a universal reference while U&& is?
+  // T&& is NOT a universal reference because T is already a fixed type (not
+  // deduced). U&& IS a universal reference because U is deduced dynamically.
+  template <typename U>
+  // Conversion (std::convertible_to<U, T>) creates a new object, so no
+  // reference is needed. Assignment (std::assignable_from<T&, U>) modifies an
+  // existing object, so T& is required. requires std::convertible_to<U, T>
+    requires std::assignable_from<T &, U>
+  bool enqueue(U &&item) {
+    // What does `std::memory_order_relaxed` mean?
+    // It means that we don't need to synchronize with other threads.
+    size_t tail = m_write_ptr.load(std::memory_order_relaxed);
+    const size_t next_tail = (tail + 1) % m_capacity;
 
-            if (next_tail == m_read_ptr.load(std::memory_order_acquire)) {
-                // tail + 1 == head , i.e., buffer is full
-                return false;
-            }
+    if (next_tail == m_read_ptr.load(std::memory_order_acquire)) {
+      // tail + 1 == head , i.e., buffer is full
+      return false;
+    }
 
-            m_buffer[tail] = std::forward<U>(item);
-            m_write_ptr.store(next_tail, std::memory_order_release);
-            return true;
-        }
+    m_buffer[tail] = std::forward<U>(item);
+    m_write_ptr.store(next_tail, std::memory_order_release);
+    return true;
+  }
 
-        bool dequeue(T &item) {
-            size_t head = m_read_ptr.load(std::memory_order_relaxed);
-            if (head == m_write_ptr.load(std::memory_order_acquire)) {
-                // head == tail, i.e., buffer is empty
-                return false;
-            }
+  bool dequeue(T &item) {
+    size_t head = m_read_ptr.load(std::memory_order_relaxed);
+    if (head == m_write_ptr.load(std::memory_order_acquire)) {
+      // head == tail, i.e., buffer is empty
+      return false;
+    }
 
-            item = std::move(m_buffer[head]);
-            m_read_ptr.store((head + 1) % m_capacity,
-                             std::memory_order_release);
-            return true;
-        }
+    item = std::move(m_buffer[head]);
+    m_read_ptr.store((head + 1) % m_capacity, std::memory_order_release);
+    return true;
+  }
 
-        [[nodiscard]] std::size_t size_approx() const {
-            const size_t tail = m_write_ptr.load(std::memory_order_acquire);
-            const size_t head = m_read_ptr.load(std::memory_order_acquire);
-            if (tail >= head)
-                return tail - head;
-            return (m_capacity + tail - head) % m_capacity;
-        }
+  [[nodiscard]] std::size_t size_approx() const {
+    const size_t tail = m_write_ptr.load(std::memory_order_acquire);
+    const size_t head = m_read_ptr.load(std::memory_order_acquire);
+    if (tail >= head)
+      return tail - head;
+    return (m_capacity + tail - head) % m_capacity;
+  }
 
-        [[nodiscard]] std::size_t capacity() const { return m_capacity - 1; }
-
-    };
-} // namespace PoC::LockFree
+  [[nodiscard]] std::size_t capacity() const { return m_capacity - 1; }
+};
+} // namespace LockFree
 
 #endif // INC_SPSC_RINGBUFFER_IMPL_H
